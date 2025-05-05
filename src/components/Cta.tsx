22b8f662f1f9372f11359ab4b4cb1ab30e2864c7
peculiar <pecuiheubammadu1st@gmail.com>
2025-05-05 16:58:17 +0100

    add components folder

---
+++ src/components/Cta.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function cta() {
+  return (
+    <div>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <p className="text-lg">This is a simple call to action component.</p>
+        <button className="bg-blue-500 text-white py-2 px-4 rounded">Click me</button>
+    </div>
+  )
+}
+
+export default cta
