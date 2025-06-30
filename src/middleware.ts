91eb7c2e92f79d84de168a7eb859b9bfc9193bbe
Christee <Christeechristopher@gmail.com>
2025-06-30 17:21:13 +0100

    change supabase instance

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,12 @@
 // middleware.ts
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
-import { supabaseClient } from "./supabase";
+import { createSupabaseServerClient } from "./app/(backend)/lib";
+
 
 export async function middleware(req: NextRequest) {
   if (req.nextUrl.pathname.startsWith("/api/auth/me")) {
-    const supabase = supabaseClient.createClient();
+   const supabase = createSupabaseServerClient();
     const { data } = await supabase.auth.getUser();
     if (!data.user) {
       return new NextResponse("You are not authorized", { status: 401 });
@@ -16,7 +17,7 @@ export async function middleware(req: NextRequest) {
 
 export const config = {
   matcher: [
-    "/api/auth/me/:path*",
+     "/api/auth/me/:path*",
     "/api/auth/logout",
     "/api/auth/profile",
     "/api/auth/change-password",
