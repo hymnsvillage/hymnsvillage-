1ab1183e1f8162959be3c2f4437266db942e0dd4
clever akanimoh <cleverakanimoh02@gmail.com>
2025-07-18 00:55:16 +0100

    modified route.ts in follow

--- src/app/(backend)/api/user/follow/route.ts
+++ src/app/(backend)/api/user/follow/route.ts
@@ -27,7 +27,10 @@ export async function POST(req: NextRequest) {
   });
 
   if (error)
-    return NextResponse.json({ error: error.message }, { status: 500 });
+    return NextResponse.json(
+      { error: "Sorry! we couldn't follow that user" },
+      { status: 500 }
+    );
 
   return NextResponse.json(
     customResponse({ message: "User followed successfully" })
