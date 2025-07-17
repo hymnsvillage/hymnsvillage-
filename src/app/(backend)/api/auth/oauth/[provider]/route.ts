65db263d211f37c83bd93ccdc9821f2d779dbac5
clever akanimoh <cleverakanimoh02@gmail.com>
2025-07-17 23:31:54 +0100

    modified route.ts in [provider]

--- src/app/(backend)/api/auth/oauth/[provider]/route.ts
+++ src/app/(backend)/api/auth/oauth/[provider]/route.ts
@@ -1,19 +1,30 @@
-// app/api/auth/oauth/[provider]/route.ts
-
-import { appUrl, supabaseClient } from "@/supabase";
+import { createSupabaseServerClient } from "@/app/(backend)/lib";
+import { appUrl } from "@/supabase";
 import { Provider } from "@supabase/supabase-js";
 import { NextResponse } from "next/server";
+
+const allowedProviders: Provider[] = ["google"];
 
 export async function GET(
   req: Request,
   { params }: { params: { provider: string } }
 ) {
-    const { provider } = params;
-    
+  const provider = params.provider.toLowerCase();
+
+  // Validate provider
+  if (!allowedProviders.includes(provider as Provider)) {
+    return NextResponse.json(
+      { error: `Unsupported provider: ${provider}` },
+      { status: 400 }
+    );
+  }
+
+  const supabase = createSupabaseServerClient()
+
   const {
     data: { url },
     error,
-  } = await supabaseClient.supabase.auth.signInWithOAuth({
+  } = await supabase.auth.signInWithOAuth({
     provider: provider.toUpperCase() as Provider,
     options: { redirectTo: `${appUrl}/auth/callback` },
   });
